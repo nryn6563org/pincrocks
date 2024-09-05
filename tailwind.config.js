c3a85aa8fc7cdc42038a12c0d8095a4497998f71
jhLee <77824683+nryn6563org@users.noreply.github.com>
2024-09-05 09:24:51 +0900

    핀크록스

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "pre" : ["Pretendard Variable", "Pretendard"],
+      "SBAggro" : ["SBAggro"],
+    },
+  },
+  plugins: [],
+}
